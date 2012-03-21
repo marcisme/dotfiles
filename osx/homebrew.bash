1044f35e0835c7800ca5f562dc9ddf1d62ced6f0
Marc Schwieterman <marc@marcisme.com>
2012-03-20 21:48:55 -0400

    make bash completion linux friendly

--- osx/homebrew.bash
+++ osx/homebrew.bash
@@ -1,7 +1,3 @@
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  source $(brew --prefix)/etc/bash_completion
-fi
-
 # path for homebrew installed executables
 PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
