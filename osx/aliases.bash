6ae4ab2de5a1538189fb37c5ce1caf12cecf3d9f
Marc Schwieterman <marc@marcisme.com>
2014-03-24 18:05:50 +0700

    osx: alias for Sketch plugin directory

--- osx/aliases.bash
+++ osx/aliases.bash
@@ -1,2 +1,3 @@
 alias ldd='otool -L'
 alias vimm='vim ~/Library/Application\ Support/MailMate/Resources/KeyBindings/Custom.plist'
+alias spd='cd ~/Library/Containers/com.bohemiancoding.sketch/Data/Library/Application\ Support/sketch/Plugins'
