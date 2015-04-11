100fa6d3ccde6666ee021cea978b57ea92c6e8a1
Marc Schwieterman <marc@marcisme.com>
2015-04-11 15:46:00 -0400

    osx: disable t function in favor of todo.txt

--- bash/functions.bash
+++ bash/functions.bash
@@ -1,31 +1,31 @@
-function t() {
-  if [ $# -lt 1 ] || ! [[ "$1" =~ ^[[:digit:]]$ ]]; then
-    echo "usage: t <num executions> [command]"
-    # remove execution from history if it was invalid
-    history -d $((HISTCMD-1))
-    return 1
-  fi
-
-  local n=$1; shift
-  local cmd=$*
-
-  # use last command in shell history if none is provided
-  if [ -z "$cmd" ]; then
-    cmd=$(fc -ln -1)
-    # no idea why this needs to be a separate command...
-    cmd=$(echo $cmd | sed 's/^ *//g')
-  fi
-
-  # reduce to base command if a previous t invocation
-  if [[ "$cmd" =~ ^[[:space:]]*t[[:space:]]+[[:digit:]]+ ]]; then
-    cmd=$(echo $cmd | sed 's/^\s*t\s\+[0-9]\+\s*//')
-  fi
-
-  echo "running '$cmd' $n times"
-  for ((i=1; i<=n; i++)); do
-    eval $cmd
-  done
-
-  # overwrite history with the expanded command
-  history -s "t $n $cmd"
-}
+# function t() {
+#   if [ $# -lt 1 ] || ! [[ "$1" =~ ^[[:digit:]]$ ]]; then
+#     echo "usage: t <num executions> [command]"
+#     # remove execution from history if it was invalid
+#     history -d $((HISTCMD-1))
+#     return 1
+#   fi
+#
+#   local n=$1; shift
+#   local cmd=$*
+#
+#   # use last command in shell history if none is provided
+#   if [ -z "$cmd" ]; then
+#     cmd=$(fc -ln -1)
+#     # no idea why this needs to be a separate command...
+#     cmd=$(echo $cmd | sed 's/^ *//g')
+#   fi
+#
+#   # reduce to base command if a previous t invocation
+#   if [[ "$cmd" =~ ^[[:space:]]*t[[:space:]]+[[:digit:]]+ ]]; then
+#     cmd=$(echo $cmd | sed 's/^\s*t\s\+[0-9]\+\s*//')
+#   fi
+#
+#   echo "running '$cmd' $n times"
+#   for ((i=1; i<=n; i++)); do
+#     eval $cmd
+#   done
+#
+#   # overwrite history with the expanded command
+#   history -s "t $n $cmd"
+# }
--- osx/aliases.bash
+++ osx/aliases.bash
@@ -1,3 +1,4 @@
 alias ldd='otool -L'
 alias vimm='vim ~/Library/Application\ Support/MailMate/Resources/KeyBindings/Custom.plist'
 alias spd='cd ~/Library/Containers/com.bohemiancoding.sketch/Data/Library/Application\ Support/sketch/Plugins'
+alias t='todo.sh'
