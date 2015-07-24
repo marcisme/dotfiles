88f5304cefeb16f1282b33e55c179325806f5313
Marc Schwieterman <marc@marcisme.com>
2015-07-24 15:19:35 -0700

    git: convert shell aliases to git aliases

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,22 +1,4 @@
 alias g="git"
-alias gs="g status"
-alias ga="g add"
-alias grm="g rm"
-alias gc="g commit"
-alias gca="gc --amend"
-alias gcA="gc --amend -C HEAD"
-alias gl="g log"
-alias glo="gl --oneline"
-alias glg="gl --graph"
-alias gln="glo --name-status"
-alias gb="g branch"
-alias gt="g tag"
-alias gco="git checkout"
-alias gd="g diff"
-alias gdc="gd --cached"
-alias gf="g fetch"
-alias gpr="g pull --rebase"
-alias gtfo="g reset --hard && g clean -fd"
 
 # make completion work for g
 # http://askubuntu.com/questions/62095/how-to-alias-git-to-g-so-that-bash-completion-rules-are-preserved
